650d0beba05fdef29c47435c9b4e57d338f6a324
Nika Ramin <128173488+NikaRamin@users.noreply.github.com>
2024-03-14 15:47:52 +0330

    Add files via upload

---
+++ arrayInPython.py
@@ -0,0 +1,11 @@
+# Write a  Python program to create an array of 5 integers 
+# and display the array items.
+# Access individual elements through indexes
+
+array = [10,5,6,7,2]
+print("Array items:",array)
+print("array[0]:",array[0])
+print("array[1]:",array[1])
+print("array[2]:",array[2])
+print("array[3]:",array[3])
+print("array[4]:",array[4])
